70555aac24636f6f803ca081a1eb4e3b99c3974f
Eugene-Gwebu <gwebueugene@gmail.com>
2024-03-23 16:41:34 +0000

    Change model name from Book to Booking

--- restaurant/models.py
+++ restaurant/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
-class Book(models.Model):
+class Booking(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
